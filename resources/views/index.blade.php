1bcd2311b5aaa58d456e2ba3554afc8e4af43bd6
akel <i.kalinin@conci.info>
2017-09-08 18:36:45 +0300

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -322,24 +322,22 @@
 {{--<script src="/js/respond.min.js"></script>--}}
 <script src="/js/script.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+
+
+
 <script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
     ga('create', 'UA-106162019-1', 'auto');
     ga('send', 'pageview');
 
 </script>
-<!— Yandex.Metrika counter —>
+
+
+
 <script type="text/javascript">
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function () {
@@ -376,6 +374,6 @@
 <noscript>
     <div><img src="https://mc.yandex.ru/watch/45889551" style="position:absolute; left:-9999px;" alt=""/></div>
 </noscript>
-<!— /Yandex.Metrika counter —>
+
 </body>
 </html>
