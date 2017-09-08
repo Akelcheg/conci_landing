9ea420901b891e2dc54e29dfd8ec944ae894dd71
akel <i.kalinin@conci.info>
2017-09-08 18:18:52 +0300

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -320,5 +320,15 @@
 {{--<script src="/js/respond.min.js"></script>--}}
 <script src="/js/script.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-106162019-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
